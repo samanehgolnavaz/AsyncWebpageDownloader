360274e36980c94b81940929f2ab7b9b92cea60c
Samaneh Golanvaz <samanehgolnavaz@gmail.com>
2024-08-27 18:30:43 +0200

    Added WebpageServices

---
+++ src/Core/AsyncWebpageDownloader.Application/AsyncWebpageDownloader.Application/Interfaces/IWebPageDownloaderService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncWebpageDownloader.Application.Interfaces
+{
+    public interface IWebPageDownloaderService
+    {
+        Task<string> DownloadWebPageAsync(string url);
+    }
+}
---
+++ src/Core/AsyncWebpageDownloader.Application/AsyncWebpageDownloader.Application/Services/WebPageDownloaderService.cs
@@ -0,0 +1,31 @@
+﻿using AsyncWebpageDownloader.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncWebpageDownloader.Application.Services
+{
+    public class WebPageDownloaderService : IWebPageDownloaderService
+    {
+        private readonly HttpClient _httpClient;
+        public WebPageDownloaderService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public async Task<string> DownloadWebPageAsync(string url)
+        {
+            try
+            {
+                HttpResponseMessage response = await _httpClient.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+                return await response.Content.ReadAsStringAsync();
+            }
+            catch (Exception ex)
+            {
+                return $"Error downloading {url}: {ex.Message}";
+            }
+        }
+    }
+}
