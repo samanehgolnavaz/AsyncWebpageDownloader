990f1379505f61d599e456580c18c39e27d51d06
Samaneh Golanvaz <samanehgolnavaz@gmail.com>
2024-08-27 21:37:04 +0200

    Added Configuration in API and WebpageDownloaderService

---
+++ src/Core/AsyncWebpageDownloader.API/AsyncWebpageDownloader.API/Middlewares/GlobalExceptionHandlerMiddleware.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Serilog;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace AsyncWebPageDownloader.API.Middleware
+{
+    public class GlobalExceptionHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly Serilog.ILogger _logger;
+
+        public GlobalExceptionHandlerMiddleware(RequestDelegate next, Serilog.ILogger logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception ex)
+            {
+                _logger.Error($"Something went wrong: {ex.Message}");
+                await HandleExceptionAsync(httpContext, ex);
+            }
+        }
+
+        private Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+            var result = new
+            {
+                context.Response.StatusCode,
+                Message = "Internal Server Error. Please try again later."
+            };
+
+            return context.Response.WriteAsync(result.ToString());
+        }
+    }
+}
