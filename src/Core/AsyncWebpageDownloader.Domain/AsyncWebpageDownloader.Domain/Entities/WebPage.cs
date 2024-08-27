386dcd8b23d1f68bc1b790742c62ab9bdf5b6a42
Samaneh Golanvaz <samanehgolnavaz@gmail.com>
2024-08-27 18:13:25 +0200

    Add project files.

---
+++ src/Core/AsyncWebpageDownloader.Domain/AsyncWebpageDownloader.Domain/Entities/WebPage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncWebpageDownloader.Domain.Entities
+{
+    public class WebPage
+    {
+        public int Url { get; set; }
+        public string Content { get; set; }
+    }
+}
